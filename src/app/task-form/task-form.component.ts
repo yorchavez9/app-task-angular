bb67b5346db6fdb4c355cd9810a144bd34192b7e
Jorge Chávez Huincho <djjmygm160399@gmail.com>
2024-10-19 09:10:18 -0500

    Validadndo el formulario

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -1,37 +1,61 @@
 import { Component } from '@angular/core';
 import { TasksService } from '../services/tasks.service';
-import { FormsModule } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.css'],
 })
 export class TaskFormComponent {
-  task: any = {
-    title: '',
-    description: '',
-    urgent: false,
-  };
+  taskForm: FormGroup;
 
-  constructor(private taskService: TasksService) {}
+  constructor(private taskService: TasksService, private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', Validators.required],
+      urgent: [false],
+    });
+  }
+
+  validateForm(): boolean {
+    let isValid = true;
+
+    // Validar título
+    if (this.taskForm.get('title')?.invalid) {
+      isValid = false;
+      console.error('El título es requerido.');
+    }
+
+    // Validar descripción
+    if (this.taskForm.get('description')?.invalid) {
+      isValid = false;
+      console.error('La descripción es requerida.');
+    }
+
+    return isValid;
+  }
 
   onSubmit() {
-    this.taskService.createTask(this.task).subscribe(
-      (response) => {
-        console.log('Tarea creada exitosamente:', response);
-        // Limpiar el formulario restableciendo el objeto task
-        this.task = {
-          title: '',
-          description: '',
-          urgent: false,
-        };
-      },
-      (error) => {
-        console.error('Error al crear la tarea:', error);
-      }
-    );
+    if (this.validateForm()) {
+      this.taskService.createTask(this.taskForm.value).subscribe(
+        (response) => {
+          console.log('Tarea creada exitosamente:', response);
+          this.taskForm.reset(); // Resetea el formulario después de enviar
+        },
+        (error) => {
+          console.error('Error al crear la tarea:', error);
+        }
+      );
+    } else {
+      console.error('Formulario inválido');
+    }
   }
 }
